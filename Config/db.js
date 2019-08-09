923f41de017094d49871391f35da63d100da31fe
polako <polako.rafa@gmail.com>
2019-08-09 00:35:01 -0300

    make it better

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,6 @@
 if (process.env.NODE_EV == 'production') {
-    console.log('desenvolvimento')
     module.exports = { mongoURI: 'mongodb+srv://rafaelzf:nega123@blogapp-dfbej.mongodb.net/test?retryWrites=true&w=majority' };
 } else {
-    console.log('produção')
     module.exports = { mongoURI: 'mongodb://localhost/blogapp' };
 
 };
